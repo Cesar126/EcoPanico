8408bcfad2eb2c8b780717085bc278571b886c3e
unknown <Administrador@SALA15-3.pucesi.edu.ec>
2026-06-03 16:50:23 -0500

    feat: configure Firebase Android with google-services.json

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
+    id("com.google.gms.google-services")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
